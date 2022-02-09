b6d8dbec67c51685f34e1ff5290e7a4036cff32d
billybutt60 <muhammad.bilal@educative.io>
2022-02-09 12:03:49 +0500

    lo gee

--- helloWorld.py
+++ helloWorld.py
@@ -1,15 +1,1 @@
-print("Hello World") 
-
-
-print("adding something")
-
-print("Do something")
-
-print("hsdaofhasdf")
-
-
-print("Usman dark mode")
-print("Usman dark mode")
-print("Usman dark mode")
-print("Usman dark mode")
-print("Usman dark mode")
+print("Only for commit")
